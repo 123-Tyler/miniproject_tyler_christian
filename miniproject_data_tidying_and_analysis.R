5c1f1f38253a91a32bea52ad5051167e77b2d0e1
Tyler <tchristianbk@gmial.com>
2024-02-12 16:03:16 +0000

    deleted old code

--- miniproject_data_tidying_and_analysis.R
+++ miniproject_data_tidying_and_analysis.R
@@ -7,35 +7,6 @@ library(tidyverse)
 library(vegan)
 library(datawizard)
 
-# iNaturalist data tidying ------------------------------------------------
-
-iNaturalist <- read.csv("iNaturalist_data.csv",
-                        na.strings = "") %>% 
-  mutate(id = 
-           as.factor(id),
-         time_zone =
-           as.factor(time_zone),
-         user_id =
-           as.factor(user_id),
-         user_name = 
-           as.factor(user_name),
-         quality_grade = 
-           as.factor(quality_grade),
-         species_guess = 
-           as.factor(species_guess),
-         scientific_name =
-           as.factor(scientific_name),
-         common_name =
-           as.factor(common_name),
-         iconic_taxon_name = 
-           as.factor(iconic_taxon_name),
-         taxon_id = 
-           as.factor(taxon_id)
-         )
-
-str(iNaturalist)
-head(iNaturalist)
-
 # gov.uk data tidying -----------------------------------------------------
 
 gov_data <- read.csv("UK_wild_birds_datasheet_2023_condensed.csv",
@@ -64,131 +35,9 @@ gov_data <- read.csv("UK_wild_birds_datasheet_2023_condensed.csv",
 str(gov_data)
 head(gov_data)
 
-# Combining the datasets --------------------------------------------------
+# iNaturalist data tidying ------------------------------------------------
 
-unique(gov_data$scientific_name) # 161 unique species
-unique(iNaturalist$scientific_name) # 112 unique species
-
-# Species list + what datasets its in:
-
-species_presence <- merge(gov_data,
-                          iNaturalist,
-                          by = 'scientific_name',
-                          all = TRUE)  %>% 
-  mutate(common_name_gov = 
-           common_name.x,
-         common_name_iNat =
-           common_name.y) %>% 
-  select(!c(common_name.x, common_name.y)) %>% 
-  select(scientific_name, common_name_gov, common_name_iNat)
-
-sum(is.na(species_presence$common_name_gov)) # 335 species missing from iNaturalist data
-sum(is.na(species_presence$common_name_iNat)) # 137 species missing from gov.uk data
-sum(is.na(gov_data$common_name))
-sum(is.na(iNaturalist$common_name)) # no blanks in original datasets, therefore issues with overlap.
-
-species_presence <- species_presence %>% 
-  mutate(present_gov =
-           ifelse(
-             !is.na(common_name_gov),
-             "yes",
-             "no"
-           ),
-         present_iNat =
-           ifelse(
-             !is.na(common_name_iNat),
-             "yes",
-             "no"
-           )) %>% 
-  select(scientific_name, present_gov, present_iNat) %>% 
-  distinct() %>% 
-  mutate(present_both = 
-           ifelse(
-             present_gov == "yes" & present_iNat == "yes",
-             "yes",
-             "no"
-           ))
-
-sum(species_presence$present_both == "yes") # 56 species in both datasets
-
-# creating a final species list
-
-final_species_list <- species_presence %>% 
-  filter(present_both == "yes")
-
-final_species_list <- as.vector(final_species_list$scientific_name)
-
-# Filtering the original datasets for species present in both:
-
-iNaturalist_both <- iNaturalist %>% 
-  mutate(present_both =          # filtering for species in both
-           ifelse(scientific_name %in% final_species_list,
-                  "yes",
-                  "no")) %>% 
-  filter(present_both ==
-           "yes") %>% 
-  mutate(year_uploaded =         # creating a year uploaded column
-           substr(observed_on, 
-                  7,
-                  10),
-         year_uploaded =
-           as.factor(year_uploaded)) %>% 
-  select(                        # filtering for useful columns
-    id,
-    user_id,
-    quality_grade,
-    url,
-    image_url,
-    sound_url,
-    tag_list,
-    description,
-    num_identification_agreements,
-    num_identification_disagreements,
-    captive_cultivated,
-    place_guess,
-    longitude,
-    latitude,
-    species_guess,
-    scientific_name,
-    common_name,
-    iconic_taxon_name,
-    taxon_id,
-    year_uploaded
-  )
-
-gov_data_both <- gov_data %>% 
-  mutate(present_both =          # filtering for species in both
-           ifelse(scientific_name %in% final_species_list,
-                  "yes",
-                  "no")) %>% 
-  filter(present_both == "yes") 
-
-# calculating % change in iNaturalist -------------------------------------
-
-unique(iNaturalist_both$year_uploaded)
-plot(iNaturalist_both$year_uploaded)  # dates from 1988 to 2023
-# gov_data has two categories (1970-2021 and 2016-2021), therefore only calculate short-term % change.
-
-length(iNaturalist_both)/length(unique(iNaturalist_both$scientific_name)) # not many repeated observations of species
-
-gov_data_both <- gov_data_both %>% 
-  select(!c(long_term_time_period:long_term_trend)) # remove long term data as dates don't match iNaturalist
-
-
-
-###########
-###########
-#########
-##########
-##########
-###########
-###########
-############
-
-
-# new data ----------------------------------------------------------------
-
-# data from 2000-2013
+# (data from 2000-2013)
 
 sp_richness <- read.csv("Species_richness_2000.csv") %>% 
   select(Location, Bird) %>% 
@@ -277,12 +126,6 @@ iNat_edited <- iNat_new %>%
              "No",
              "Yes"
            ),
-         tag_list_present = 
-           ifelse(
-             is.na(tag_list),
-             "No",
-             "Yes"
-           ),
          description_present = 
            ifelse(
              is.na(description),
@@ -292,10 +135,27 @@ iNat_edited <- iNat_new %>%
          species_guess_same_common =
            ifelse(species_guess == common_name,
                   "Yes",
-                  "no")) %>% 
+                  "no"),
+         id = 
+           as.factor(id),
+         user_id =
+           as.factor(user_id),
+         quality_grade = 
+           as.factor(quality_grade),
+         species_guess = 
+           as.factor(species_guess),
+         scientific_name =
+           as.factor(scientific_name),
+         common_name =
+           as.factor(common_name),
+         iconic_taxon_name = 
+           as.factor(iconic_taxon_name),
+         taxon_id = 
+           as.factor(taxon_id),
+         taxon_family_name = 
+           as.factor(taxon_family_name)) %>% 
   select(!c(
     observed_on,
-    place_guess:place_county_name,
     iconic_taxon_name,
     taxon_id
   ))
